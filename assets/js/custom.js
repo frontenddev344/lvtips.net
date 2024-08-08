6efc96d1171ad62c7ccfc20e452764842cd3c527
devavology <145644105+devavology@users.noreply.github.com>
2024-08-08 18:26:33 +0530

    done

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,78 +1,129 @@
-// $(".toggle").click(function(){
-
-//   $("header").toggleClass("menuToggle");
-
-// });
-
-$(".toggle").click(function() {
-    $("header").addClass("menuToggle");
-});
-$(".close").click(function() {
-    $("header").removeClass("menuToggle");
-});
 
 
 
 
 
-$(window).scroll(function(){
-    if ($(window).scrollTop() >= 30) {
-        $('header').addClass('fixed-header');
-        $('header').addClass('visible-title');
-    }
-    else {
-        $('header').removeClass('fixed-header');
-        $('header').removeClass('visible-title');
-    }
-});
 
 
 
 
 
-// $('.contless-button').click(function() {
-//   $('.moretextt').slideToggle();
-//   if ($('.contless-button').text() == "En savoir plus") {
-//     $(this).text("lire moins")
-//   } else {
-//     $(this).text("En savoir plus")
-//   }
-// });
 
-$('.contless-button').click(function() {
-  $(this).prev('.moretextt').slideToggle();
-  if ($('.contless-button').text() == "En savoir plus") {
-    $(this).text("lire moins")
-  } else {
-    $(this).text("En savoir plus")
+
+class Accordion {
+  constructor(el) {
+    this.el = el;
+    this.summary = el.querySelector("summary");
+    this.content = el.querySelector(".accordion-content");
+    this.expandIcon = this.summary.querySelector(".accordion-icon");
+    this.animation = null;
+    this.isClosing = false;
+    this.isExpanding = false;
+    this.summary.addEventListener("click", (e) => this.onClick(e));
   }
-});
 
-// $('.contless-button').click(function() {
-//   $(this).next('.moretextt').slideToggle();
-//   if ($(this).text() === "En savoir plus") {
-//     $(this).text("lire moins");
-//   } else {
-//     $(this).text("En savoir plus");
-//   }
-// });
-// $('.contless-button').click(function() {
-//   $(this).next('.moretextt').slideToggle();
-//   if ($(this).text() === "En savoir plus") {
-//     $(this).text("lire moins");
-//   } else {
-//     $(this).text("En savoir plus");
-//   }
-// });
+  onClick(e) {
+    e.preventDefault();
+    this.el.style.overflow = "hidden";
 
-$('.moreless-button').click(function() {
-  $('.moretext').slideToggle();
-  if ($('.moreless-button').text() == "En savoir plus") {
-    $(this).text("lire moins")
-  } else {
-    $(this).text("En savoir plus")
+    if (this.isClosing || !this.el.open) {
+      this.open();
+    } else if (this.isExpanding || this.el.open) {
+      this.shrink();
+    }
   }
+
+  shrink() {
+    this.isClosing = true;
+
+    const startHeight = `${this.el.offsetHeight}px`;
+    const endHeight = `${this.summary.offsetHeight}px`;
+
+    if (this.animation) {
+      this.animation.cancel();
+    }
+
+    this.animation = this.el.animate(
+      {
+        height: [startHeight, endHeight]
+      },
+      {
+        duration: 400,
+        easing: "ease-out"
+      }
+    );
+    this.animation.onfinish = () => {
+      this.expandIcon.setAttribute(
+        "src",
+        "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'><path d='M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z'/></svg>"
+      );
+      return this.onAnimationFinish(false);
+    };
+
+    this.animation.oncancel = () => {
+      this.expandIcon.setAttribute(
+        "src",
+        "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'><path d='M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z'/></svg>"
+      );
+      return (this.isClosing = false);
+    };
+  }
+
+  open() {
+    this.el.style.height = `${this.el.offsetHeight}px`;
+    this.el.open = true;
+    window.requestAnimationFrame(() => this.expand());
+  }
+
+  expand() {
+    this.isExpanding = true;
+
+    const startHeight = `${this.el.offsetHeight}px`;
+    const endHeight = `${
+      this.summary.offsetHeight + this.content.offsetHeight
+    }px`;
+
+    if (this.animation) {
+      this.animation.cancel();
+    }
+
+    this.animation = this.el.animate(
+      {
+        height: [startHeight, endHeight]
+      },
+      {
+        duration: 350,
+        easing: "ease-out"
+      }
+    );
+
+    this.animation.onfinish = () => {
+      this.expandIcon.setAttribute(
+        "src",
+        "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'><path d='M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z'/></svg>"
+      );
+      return this.onAnimationFinish(true);
+    };
+    this.animation.oncancel = () => {
+      this.expandIcon.setAttribute(
+        "src",
+        "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'><path d='M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z'/></svg>"
+      );
+      return (this.isExpanding = false);
+    };
+  }
+
+  onAnimationFinish(open) {
+    this.el.open = open;
+    this.animation = null;
+    this.isClosing = false;
+    this.isExpanding = false;
+    this.el.style.height = this.el.style.overflow = "";
+  }
+}
+
+document.querySelectorAll("details").forEach((el) => {
+  new Accordion(el);
 });
-AOS.init({
-    duration: 1200,
-  })
+
+
